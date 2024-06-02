83ce3fa400ddf99e1802cd867c84724970a3087f
gabriele.serafini <gabriele.serafini@inflead.com>
2024-06-02 18:13:05 +0200

    feat: change functionality

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
 
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
-app.use('/proxy', createProxyMiddleware({
-  target: 'https://scontent-atl3-1.cdninstagram.com',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/proxy': '',
-  },
-  onProxyReq: (proxyReq, req, res) => {
-    console.log('Proxying request to:', req.url);
-  },
-  onProxyRes: (proxyRes, req, res) => {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    proxyRes.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS';
-    proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
-  },
-  onError: (err, req, res) => {
-    console.error('Error proxying request:', err);
-    res.status(500).send('Proxy error');
+app.get('/image', async (req, res) => {
+  const imageUrl = req.query.url; 
+
+  try {
+    const response = await axios({
+      url: imageUrl,
+      method: 'GET',
+      responseType: 'stream'
+    });
+
+    res.setHeader('Content-Type', response.headers['content-type']);
+
+    response.data.pipe(res);
+  } catch (error) {
+    console.error('Error fetching image:', error);
+    res.status(500).send('Error fetching image');
   }
-}));
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+module.exports = app;
