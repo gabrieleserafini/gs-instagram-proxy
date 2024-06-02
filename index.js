dfb0afb8691243346f4a13d01e95fd3ae42777b0
gabriele.serafini <gabriele.serafini@inflead.com>
2024-06-02 17:51:28 +0200

    feat: add origins

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use('/proxy', createProxyMiddleware({
   target: 'https://scontent-atl3-1.cdninstagram.com',
   changeOrigin: true,
